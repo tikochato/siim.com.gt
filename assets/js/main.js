c2c1775f110f0226ec2b2e426ad081b630e2a36b
Tikochato <daor01@gmail.com>
2018-11-11 22:14:22 -0600

    Menu sticky

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,66 +6,121 @@
 
 (function($) {
 
-	// Breakpoints.
-		skel.breakpoints({
-			xlarge:	'(max-width: 1680px)',
-			large:	'(max-width: 1280px)',
-			medium:	'(max-width: 980px)',
-			small:	'(max-width: 736px)',
-			xsmall:	'(max-width: 480px)'
-		});
+  // Breakpoints.
+  skel.breakpoints({xlarge: '(max-width: 1680px)', large: '(max-width: 1280px)', medium: '(max-width: 980px)', small: '(max-width: 736px)', xsmall: '(max-width: 480px)'});
 
-	$(function() {
+  $(function() {
 
-		var	$window = $(window),
-			$body = $('body');
+    var $window = $(window),
+      $body = $('body'),
+      $main = $('#main');
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
+			    // Nav.
+			    var $nav = $('#nav');
+					var $navinner = $('#navinner');
+					var $logo = $('#logo');
+			    if ($nav.length > 0) {
+			      // Shrink effect.
+			      $main.scrollex({
+			        mode: 'top',
+			        enter: function() {
+			          $nav.addClass('alt');
+								$logo.addClass('alt');
+								$navinner.addClass('alt');
+								$navinner.removeClass('inner');
+			        },
+			        leave: function() {
+			          $nav.removeClass('alt');
+								$logo.removeClass('alt');
+								$navinner.removeClass('alt');
+								$navinner.addClass('inner');
+			        }
+			      });
+			      // Links.
+			      var $nav_a = $nav.find('a');
+			      $nav_a.scrolly({
+			        speed: 1000,
+			        offset: function() {
+			          return $nav.height();
+			        }
+			      }).on('click', function() {
+			        var $this = $(this);
+			        // External link? Bail.
+			        if ($this.attr('href').charAt(0) != '#')
+			          return;
+			        // Deactivate all links.
+			        $nav_a.removeClass('active').removeClass('active-locked');
+			        // Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+			        $this.addClass('active').addClass('active-locked');
+			      }).each(function() {
+			        var $this = $(this),
+			          id = $this.attr('href'),
+			          $section = $(id);
+			        // No section for this link? Bail.
+			        if ($section.length < 1)
+			          return;
+			        // Scrollex.
+			        $section.scrollex({
+			          mode: 'middle',
+			          initialize: function() {
+			            // Deactivate section.
+			            if (skel.canUse('transition'))
+			              $section.addClass('inactive');
 
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+			            }
+			          ,
+			          enter: function() {
+			            // Activate section.
+			            $section.removeClass('inactive');
+			            // No locked links? Deactivate all links and activate this section's one.
+			            if ($nav_a.filter('.active-locked').length == 0) {
+			              $nav_a.removeClass('active');
+			              $this.addClass('active');// Otherwise, if this section's link is the one that's locked, unlock it.);
+			            } else if ($this.hasClass('active-locked'))
+			              $this.removeClass('active-locked');
+			            }
+			          });
+			      });
+			    }
 
-	// Off-Canvas Navigation.
+    // Disable animations/transitions until the page has loaded.
+    $body.addClass('is-loading');
 
-		// Navigation Panel.
-			$(
-				'<div id="navPanel">' +
-					$('#nav').html() +
-					'<a href="#navPanel" class="close"></a>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left'
-				});
+    $window.on('load', function() {
+      window.setTimeout(function() {
+        $body.removeClass('is-loading');
+      }, 100);
+    });
 
-		// Fix: Remove transitions on WP<10 (poor/buggy performance).
-			if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-				$('#navPanel')
-					.css('transition', 'none');
+    // Prioritize "important" elements on medium.
+    skel.on('+medium -medium', function() {
+      $.prioritize('.important\\28 medium\\29', skel.breakpoint('medium').active);
+    });
 
-	});
+    // Off-Canvas Navigation.
+
+    // Navigation Panel.
+    $('<div id="navPanel">' + $('#nav').html() + '<a href="#navPanel" class="close"></a>' + '</div>').appendTo($body).panel({
+      delay: 500,
+      hideOnClick: true,
+      hideOnSwipe: true,
+      resetScroll: true,
+      resetForms: true,
+      side: 'left'
+    });
+
+
+    // Fix: Remove transitions on WP<10 (poor/buggy performance).
+    if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+      $('#navPanel').css('transition', 'none');
+
+    }
+  );
 
 })(jQuery);
 
 /* carrousel */
 $(document).ready(function() {
-    $('.pgwSlideshow').pgwSlideshow();
+  $('.pgwSlideshow').pgwSlideshow();
 });
